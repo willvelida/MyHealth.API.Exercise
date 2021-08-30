58b6c7e43dcac3d9d5467ae2c4f4ab89619a1c0d
willvelida <willvelida@hotmail.co.uk>
2021-08-30 19:34:04 +1200

    Adding unit tests for ExerciseDbService

---
+++ MyHealth.API.Exercise/MyHealth.API.Exercise.UnitTests/ServiceTests/ExerciseDbServiceShould.cs
@@ -0,0 +1,134 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using MyHealth.API.Exercise.Services;
+using MyHealth.API.Exercise.UnitTests.TestExtensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using mdl = MyHealth.Common.Models;
+
+namespace MyHealth.API.Exercise.UnitTests.ServiceTests
+{
+    public class ExerciseDbServiceShould
+    {
+        private readonly Mock<CosmosClient> _cosmosClientMock;
+        private readonly Mock<Container> _containerMock;
+        private readonly Mock<IConfiguration> _configMock;
+
+        private ExerciseDbService _sut;
+
+        public ExerciseDbServiceShould()
+        {
+            _cosmosClientMock = new Mock<CosmosClient>();
+            _containerMock = new Mock<Container>();
+            _cosmosClientMock.Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>())).Returns(_containerMock.Object);
+            _configMock = new Mock<IConfiguration>();
+            _configMock.Setup(x => x["BookDatabaseName"]).Returns("db");
+            _configMock.Setup(x => x["BookContainerName"]).Returns("col");
+
+            _sut = new ExerciseDbService(_configMock.Object, _cosmosClientMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateWorkoutItemSuccessfully()
+        {
+            var fixture = new Fixture();
+            var testWorkout = fixture.Create<mdl.ExerciseEnvelope>();
+
+            _containerMock.SetupCreateItemAsync<mdl.ExerciseEnvelope>();
+
+            Func<Task> createWorkoutAction = async () => await _sut.CreateWorkout(testWorkout);
+
+            await createWorkoutAction.Should().NotThrowAsync<Exception>();
+            _containerMock.Verify(x => x.CreateItemAsync(
+                It.IsAny<mdl.ExerciseEnvelope>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task DeleteWorkoutItemSuccessfully()
+        {
+            var fixture = new Fixture();
+            var testWorkout = fixture.Create<mdl.ExerciseEnvelope>();
+
+            _containerMock.SetupDeleteItemAsync<mdl.ExerciseEnvelope>();
+
+            Func<Task> deleteWorkoutAction = async () => await _sut.DeleteWorkout(testWorkout);
+
+            await deleteWorkoutAction.Should().NotThrowAsync<Exception>();
+            _containerMock.Verify(x => x.DeleteItemAsync<mdl.ExerciseEnvelope>(
+                It.IsAny<string>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task GetAllWorkoutItemsSuccessfully()
+        {
+            var fixture = new Fixture();
+            var testWorkout = fixture.Create<mdl.ExerciseEnvelope>();
+            List<mdl.ExerciseEnvelope> testExercises = new List<mdl.ExerciseEnvelope>();
+            testExercises.Add(testWorkout);
+
+            _containerMock.SetupItemQueryIteratorMock(testExercises);
+            _containerMock.SetupItemQueryIteratorMock(new List<int> { 1 });
+
+            var response = await _sut.GetAllWorkouts();
+
+            using (new AssertionScope())
+            {
+                response[0].Date.Should().Be(testWorkout.Date);
+                response[0].DocumentType.Should().Be(testWorkout.DocumentType);
+            }
+
+            Assert.Equal(testExercises.Count, response.Count);
+        }
+
+        [Fact]
+        public async Task GetWorkoutByDateSuccessfully()
+        {
+            var fixture = new Fixture();
+            var testWorkout = fixture.Create<mdl.ExerciseEnvelope>();
+            List<mdl.ExerciseEnvelope> testExercises = new List<mdl.ExerciseEnvelope>();
+            testExercises.Add(testWorkout);
+
+            _containerMock.SetupItemQueryIteratorMock(testExercises);
+            _containerMock.SetupItemQueryIteratorMock(new List<int> { 1 });
+
+            var response = await _sut.GetWorkoutByDate(testWorkout.Date);
+
+            using (new AssertionScope())
+            {
+                response.Date.Should().Be(testWorkout.Date);
+                response.DocumentType.Should().Be(testWorkout.DocumentType);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateWorkoutItemSuccessfully()
+        {
+            var fixture = new Fixture();
+            var testWorkout = fixture.Create<mdl.ExerciseEnvelope>();
+
+            _containerMock.SetupReplaceItemAsync<mdl.ExerciseEnvelope>();
+
+            await _sut.UpdateWorkout(testWorkout);
+            _containerMock.Verify(x => x.ReplaceItemAsync(
+                It.IsAny<mdl.ExerciseEnvelope>(),
+                It.IsAny<string>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()));
+        }
+    }
+}
---
+++ MyHealth.API.Exercise/MyHealth.API.Exercise.UnitTests/TestExtensions/CosmosExtensions.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Azure.Cosmos;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MyHealth.API.Exercise.UnitTests.TestExtensions
+{
+    public static class CosmosExtensions
+    {
+        public static Mock<ItemResponse<T>> SetupCreateItemAsync<T>(this Mock<Container> containerMock)
+        {
+            var itemResponseMock = new Mock<ItemResponse<T>>();
+
+            containerMock
+                .Setup(x => x.CreateItemAsync(
+                    It.IsAny<T>(),
+                    It.IsAny<PartitionKey>(),
+                    It.IsAny<ItemRequestOptions>(),
+                    It.IsAny<CancellationToken>()))
+                .Callback((T item, PartitionKey? partitionKey, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Setup(x => x.Resource).Returns(null))
+                .ReturnsAsync((T item, PartitionKey? partitionKey, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Object);
+
+            return itemResponseMock;
+        }
+
+        public static (Mock<FeedResponse<T>> feedResponseMock, Mock<FeedIterator<T>> feedIterator) SetupItemQueryIteratorMock<T>(this Mock<Container> containerMock, IEnumerable<T> itemsToReturn)
+        {
+            var feedRepsonseMock = new Mock<FeedResponse<T>>();
+            feedRepsonseMock.Setup(x => x.Resource).Returns(itemsToReturn);
+            var iteratorMock = new Mock<FeedIterator<T>>();
+            iteratorMock.SetupSequence(x => x.HasMoreResults).Returns(true).Returns(false);
+            iteratorMock.Setup(x => x.ReadNextAsync(It.IsAny<CancellationToken>())).ReturnsAsync(feedRepsonseMock.Object);
+            containerMock.Setup(x => x.GetItemQueryIterator<T>(It.IsAny<QueryDefinition>(), It.IsAny<string>(), It.IsAny<QueryRequestOptions>()))
+                .Returns(iteratorMock.Object);
+
+            return (feedRepsonseMock, iteratorMock);
+        }
+
+        public static Mock<ItemResponse<T>> SetupDeleteItemAsync<T>(this Mock<Container> containerMock)
+        {
+            var itemResponseMock = new Mock<ItemResponse<T>>();
+
+            containerMock.Setup(x => x.DeleteItemAsync<T>(
+                It.IsAny<string>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()))
+                .Callback((string id, PartitionKey? pk, ItemRequestOptions opt, CancellationToken ct) => itemResponseMock.Setup(x => x.Resource).Returns(null))
+                .ReturnsAsync((string id, PartitionKey? pk, ItemRequestOptions opt, CancellationToken ct) => itemResponseMock.Object);
+
+
+            return itemResponseMock;
+        }
+
+        public static Mock<ItemResponse<T>> SetupReplaceItemAsync<T>(this Mock<Container> containerMock)
+        {
+            var itemResponseMock = new Mock<ItemResponse<T>>();
+
+            containerMock.Setup(x => x.ReplaceItemAsync(
+                It.IsAny<T>(),
+                It.IsAny<string>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()))
+                .Callback((T item, string id, PartitionKey? pk, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Setup(x => x.Resource).Returns(null))
+                .ReturnsAsync((T item, string id, PartitionKey? pk, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Object);
+
+            return itemResponseMock;
+        }
+
+        public static Mock<ItemResponse<T>> SetupReadItemAsync<T>(this Mock<Container> containerMock, T itemToReturn)
+        {
+            var itemResponseMock = new Mock<ItemResponse<T>>();
+
+            containerMock.Setup(x => x.ReadItemAsync<T>(
+                It.IsAny<string>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()))
+                .Callback((string id, PartitionKey? pk, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Setup(x => x.Resource).Returns(itemToReturn))
+                .ReturnsAsync((string id, PartitionKey? pk, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Object);
+
+            return itemResponseMock;
+        }
+
+        public static Mock<ItemResponse<T>> SetupReadItemAsyncNotFound<T>(this Mock<Container> containerMock)
+        {
+            var itemResponseMock = new Mock<ItemResponse<T>>();
+
+            containerMock.Setup(x => x.ReadItemAsync<T>(
+                It.IsAny<string>(),
+                It.IsAny<PartitionKey>(),
+                It.IsAny<ItemRequestOptions>(),
+                It.IsAny<CancellationToken>()))
+                .Callback((string id, PartitionKey? pk, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Setup(x => x.StatusCode).Returns(System.Net.HttpStatusCode.NotFound))
+                .ReturnsAsync((string id, PartitionKey? pk, ItemRequestOptions opts, CancellationToken ct) => itemResponseMock.Object);
+
+            return itemResponseMock;
+        }
+    }
+}
